99ae68196e1d368907b2c7855af3109d532701d0
sandsmaster <www_123_val@abv.bg>
2023-03-04 22:27:35 +0200

    Fix Sportist model (missing fields)

--- reviewing/models.py
+++ reviewing/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Sportist(models.Model):
-    def build(self):
-        self.name = models.CharField(max_length=70)
-        self.age = models.IntegerField()
-        self.sport = models.CharField(max_length=48)
-        self.bio = models.TextField()
-        self.still_playing = models.BooleanField()
+    name = models.CharField(max_length=70, null=False)
+    age = models.IntegerField(default=(-1))
+    sport = models.CharField(max_length=48)
+    bio = models.TextField(blank=True, null=True)
+    still_playing = models.BooleanField()
+
+    def __str__(self):
+        return self.name
