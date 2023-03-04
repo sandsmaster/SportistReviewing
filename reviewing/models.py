edc98875906a4521ffbc007a27a5791e62c3cbcf
sandsmaster <www_123_val@abv.bg>
2023-03-04 21:37:36 +0200

    Add Sportist model with migration

--- reviewing/models.py
+++ reviewing/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Sportist(models.Model):
+    def build(self):
+        self.name = models.CharField(max_length=70)
+        self.age = models.IntegerField()
+        self.sport = models.CharField(max_length=48)
+        self.bio = models.TextField()
+        self.still_playing = models.BooleanField()
